27a5bf0cc08799e3aa3ae2f8307b5de74de68475
nkobaidze <nika.kobaidze@gmail.com>
2023-03-27 18:00:38 +0400

    [ADD] conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import styles from './App.module.css';
 
 const User = (props) => {
   return (
@@ -14,27 +14,24 @@ const User = (props) => {
 
 function App() {
   const name = "nika kobaidze";
-  const car = <h1>Subariki</h1>;
 
   const props = {
     firstName: "nika",
     lastName: "giorgadze111",
     age: "21"
   }
+
+  const isGreen = true;
+
   return (
-    <div className="App">
+    <div className={styles.App}>
       <h1>HELLO REACT</h1>
       <p>{name}</p>
       <User {...props} />
-      {/* <User /> */}
-    </div>
+      {props.age > 18 ? <h1>AGE MORE 18</h1> : <h1>UNDER 18</h1>}
+      {props.age && <h2 style={{ color: 'red' }} > DISPLAY</h2>}
+    </div >
   );
-}
-
-
-// every component must start capital letter
-const GetNameComponent = () => {
-  return <h2>NIKOO</h2>;
 }
 
 
