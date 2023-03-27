ac7cce8c2a0470d9b5d48bf0d25ddd780452b2a5
nkobaidze <nika.kobaidze@gmail.com>
2023-03-27 13:53:31 +0400

    [ADD] components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
 
-function App() {
+const User = (props) => {
   return (
-    <div className="App">HELLO REACT</div>
+    <>
+      <h1>{props.firstName}</h1>
+      <h1>{props.lastName}</h1>
+      <h1>{props.age}</h1>
+      <hr />
+    </>
   );
 }
+
+
+function App() {
+  const name = "nika kobaidze";
+  const car = <h1>Subariki</h1>;
+
+  const props = {
+    firstName: "nika",
+    lastName: "giorgadze111",
+    age: "21"
+  }
+  return (
+    <div className="App">
+      <h1>HELLO REACT</h1>
+      <p>{name}</p>
+      <User {...props} />
+      {/* <User /> */}
+    </div>
+  );
+}
+
+
+// every component must start capital letter
+const GetNameComponent = () => {
+  return <h2>NIKOO</h2>;
+}
+
+
 
 export default App;
