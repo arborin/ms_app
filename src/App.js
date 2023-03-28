cd5570917eace2067291cb6e36d9cab3d5cf8943
nkobaidze <nika.kobaidze@gmail.com>
2023-03-28 16:48:25 +0400

    [ADD] react hooks

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
-import styles from './App.module.css';
-
-const User = (props) => {
-  return (
-    <>
-      <h1>{props.firstName}</h1>
-      <h1>{props.lastName}</h1>
-      <h1>{props.age}</h1>
-      <hr />
-    </>
-  );
-}
-
+// import styles from './App.module.css';
+import { useState } from 'react'
 
 function App() {
-  const name = "nika kobaidze";
 
-  const props = {
-    firstName: "nika",
-    lastName: "giorgadze111",
-    age: "21"
-  }
+  const [counter, setCounter] = useState(0);
 
-  const isGreen = true;
 
   return (
-    <div className={styles.App}>
-      <h1>HELLO REACT</h1>
-      <p>{name}</p>
-      <User {...props} />
-      {props.age > 18 ? <h1>AGE MORE 18</h1> : <h1>UNDER 18</h1>}
-      {props.age && <h2 style={{ color: 'red' }} > DISPLAY</h2>}
-    </div >
+    <div>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Increase</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrease</button>
+      <button onClick={() => setCounter(0)}>Set To Zero</button>
+    </div>
   );
 }
 
